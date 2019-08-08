ba4426082e8282075dfffbafed09c7d5a409f887
Tiago Brito <tiago@avenuestud.io>
2019-08-08 13:23:34 +1000

    Stylled login page and add react router dom dependency

--- frontEnd/frontend/src/pages/login.js
+++ frontEnd/frontend/src/pages/login.js
@@ -1,6 +1,16 @@
 import React from "react";
+import "./Login.css";
+
 import logo from "../assets/logo.svg";
 
 export default function Login() {
-  return <img src={logo} alt="Tindev Logo" />;
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Tindev Logo" />
+        <input placeholder="Digite seu usuario do Github" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
